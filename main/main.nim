45dee3eea1788ba8f0f901c3c65bf821fb1b967b
d.terlyakhin <blackmius@gmail.com>
2021-07-11 02:58:45 +0300

    optimize build

--- main/main.nim
+++ main/main.nim
@@ -1,7 +1,6 @@
 import nesper, nesper/[net_utils, events, tasks, esp/net/tcpip_adapter]
 import asyncdispatch
-
-import networking, dns, http#, fs
+import networking, dns, http, fs
 
 const TAG: cstring = "main"
 
@@ -17,12 +16,12 @@ app_main:
   let ap_ip = toIpAddress(info.ip)
   logi TAG, "ip: %s", $ap_ip  
 
-  #let keys = readKeys()
-  #if keys.len == 0:
-  wifiApStart()
-  waitFor run_http_server() and run_dns_server(ap_ip)
-  #else:
-  #  let key = keys[0]
-  #  wifiStaStart(key.ssid, key.password)
+  let keys = readKeys()
+  if keys.len == 0:
+    wifiApStart()
+    waitFor run_http_server() and run_dns_server(ap_ip)
+  else:
+    let key = keys[0]
+    wifiStaStart(key.ssid, key.password)
 
   logi TAG, "Connection set"
